7880a4933d7e9c1add856a1dc4db18cac2dc44a2
Hadeus7712 <anibro.gl@gmail.com>
2021-11-04 21:28:47 +0300

    Добавьте файлы проекта.

---
+++ FiguresLibrary/Point.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiguresLibrary
+{
+    public class Point
+    {
+        public double x;
+        public double y;
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double Len(Point pnt)
+        {
+            return Math.Sqrt(Math.Pow(x - pnt.x, 2) + Math.Pow(y - pnt.y, 2));
+        }
+    }
+}
